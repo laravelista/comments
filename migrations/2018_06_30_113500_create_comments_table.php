2efcb5cf01ea920f55182749dc0e9fb15731f619
Mario Bašić <mario@laravelista.hr>
2018-07-14 20:11:06 +0200

    Fix for DB migration issue increments to bigIncrements.

--- migrations/2018_06_30_113500_create_comments_table.php
+++ migrations/2018_06_30_113500_create_comments_table.php
@@ -14,7 +14,7 @@ class CreateCommentsTable extends Migration
     public function up()
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->unsignedBigInteger('commenter_id');
             $table->morphs('commentable');
             $table->text('comment');
