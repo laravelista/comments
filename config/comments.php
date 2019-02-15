97c07136bf222105ec91a0d655f5d08d76ee0bbf
Damir Grgic <damir@socialveo.com>
2019-02-15 11:50:34 +0100

    For new CommentPolicy

--- config/comments.php
+++ config/comments.php
@@ -2,5 +2,12 @@
 
 return [
     // The model which creates the comments aka the User model
-    'commenter' => \App\User::class
+    'commenter' => \App\User::class,
+
+    'permissions' => [
+        'create-comment' => 'Laravelista\Comments\CommentPolicy@create',
+        'delete-comment' => 'Laravelista\Comments\CommentPolicy@delete',
+        'edit-comment' => 'Laravelista\Comments\CommentPolicy@update',
+        'reply-to-comment' => 'Laravelista\Comments\CommentPolicy@reply',
+    ]
 ];
