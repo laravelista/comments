bcd8100002c5328db149d0cb63c5b6b5a47757ce
Robin Cramer <mail@robincramer.de>
2019-07-13 18:15:23 +0200

    Add basic spam protection

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,8 @@
 <?php
 
-Route::post('comments', config('comments.controller') . '@store');
+use Spatie\Honeypot\ProtectAgainstSpam;
+
+Route::post('comments', config('comments.controller') . '@store')->middleware(ProtectAgainstSpam::class);
 Route::delete('comments/{comment}', config('comments.controller') . '@destroy');
 Route::put('comments/{comment}', config('comments.controller') . '@update');
 Route::post('comments/{comment}', config('comments.controller') . '@reply');
