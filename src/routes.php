29473c0426b68c15dc3a2f5202060afc185b63f4
feycot <feycot@gmail.com>
2020-02-08 02:55:54 +0500

    add named routes

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::post('comments', config('comments.controller') . '@store');
-Route::delete('comments/{comment}', config('comments.controller') . '@destroy');
-Route::put('comments/{comment}', config('comments.controller') . '@update');
-Route::post('comments/{comment}', config('comments.controller') . '@reply');
+Route::post('comments', config('comments.controller') . '@store')->name('comments.store');
+Route::delete('comments/{comment}', config('comments.controller') . '@destroy')->name('comments.destroy');
+Route::put('comments/{comment}', config('comments.controller') . '@update')->name('comments.update');
+Route::post('comments/{comment}', config('comments.controller') . '@reply')->name('comments.reply');
