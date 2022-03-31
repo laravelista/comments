f89477bb7cdd3cfe09a5de837229f4ee85e9ea5e
S-DICKSON <52682455+S-DICKSON@users.noreply.github.com>
2022-03-31 13:06:33 +0100

    Updated blade to show deleted at time

--- resources/views/_comment.blade.php
+++ resources/views/_comment.blade.php
@@ -1,121 +1,144 @@
 @inject('markdown', 'Parsedown')
-@php
-    // TODO: There should be a better place for this.
-    $markdown->setSafeMode(true);
-@endphp
+@php($markdown->setSafeMode(true))
 
-<div id="comment-{{ $comment->getKey() }}" class="media">
-    <img class="mr-3" src="https://www.gravatar.com/avatar/{{ md5($comment->commenter->email ?? $comment->guest_email) }}.jpg?s=64" alt="{{ $comment->commenter->name ?? $comment->guest_name }} Avatar">
-    <div class="media-body">
-        <h5 class="mt-0 mb-1">{{ $comment->commenter->name ?? $comment->guest_name }} <small class="text-muted">- {{ $comment->created_at->diffForHumans() }}</small></h5>
-        <div style="white-space: pre-wrap;">{!! $markdown->line($comment->comment) !!}</div>
+@php($isCommentDeleted = !is_null($comment->deleted_at))
 
-        <div>
-            @can('reply-to-comment', $comment)
-                <button data-toggle="modal" data-target="#reply-modal-{{ $comment->getKey() }}" class="btn btn-sm btn-link text-uppercase">@lang('comments::comments.reply')</button>
-            @endcan
-            @can('edit-comment', $comment)
-                <button data-toggle="modal" data-target="#comment-modal-{{ $comment->getKey() }}" class="btn btn-sm btn-link text-uppercase">@lang('comments::comments.edit')</button>
-            @endcan
-            @can('delete-comment', $comment)
-                <a href="{{ route('comments.destroy', $comment->getKey()) }}" onclick="event.preventDefault();document.getElementById('comment-delete-form-{{ $comment->getKey() }}').submit();" class="btn btn-sm btn-link text-danger text-uppercase">@lang('comments::comments.delete')</a>
-                <form id="comment-delete-form-{{ $comment->getKey() }}" action="{{ route('comments.destroy', $comment->getKey()) }}" method="POST" style="display: none;">
-                    @method('DELETE')
-                    @csrf
-                </form>
-            @endcan
-        </div>
+@if(isset($reply) && $reply === true)
+    <div id="comment-{{ $comment->id }}" class="media">
+        @else
+            <li id="comment-{{ $comment->id }}" class="media">
+                @endif
+                <img class="mr-3"
+                     src="https://www.gravatar.com/avatar/{{ md5($comment->commenter->email ?? $comment->guest_email) }}.jpg?s=64"
+                     alt="{{ $comment->commenter->name ?? $comment->guest_name }} Avatar">
+                <div class="media-body">
+                    <h5 class="mt-0 mb-1">{{ $comment->commenter->name ?? $comment->guest_name }}
+                        @if($isCommentDeleted)
+                            <small class="text-muted"> - Deleted {{ $comment->deleted_at->diffForHumans() }} </small>
+                        @else
+                            <small class="text-muted"> - {{ $comment->created_at->diffForHumans() }} </small>
+                        @endif
+                    </h5>
+                    <div style="white-space: pre-wrap;">{!! $markdown->line($comment->comment) !!}</div>
 
-        @can('edit-comment', $comment)
-            <div class="modal fade" id="comment-modal-{{ $comment->getKey() }}" tabindex="-1" role="dialog">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <form method="POST" action="{{ route('comments.update', $comment->getKey()) }}">
-                            @method('PUT')
-                            @csrf
-                            <div class="modal-header">
-                                <h5 class="modal-title">@lang('comments::comments.edit_comment')</h5>
-                                <button type="button" class="close" data-dismiss="modal">
-                                <span>&times;</span>
+                    <div>
+                        @if ( !isset($ticket) || $ticket->status->name != 'Closed' )
+                            @can('reply-to-comment', $comment)
+                                @if(!$isCommentDeleted)
+                                    <button data-toggle="modal" data-target="#reply-modal-{{ $comment->id }}"
+                                            class="btn btn-sm btn-link text-uppercase">Reply
+                                    </button>
+                                @endif
+                            @endcan
+                            @can('edit-comment', $comment)
+                                <button data-toggle="modal" data-target="#comment-modal-{{ $comment->id }}"
+                                        class="btn btn-sm btn-link text-uppercase">Edit
                                 </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="message">@lang('comments::comments.update_your_message_here')</label>
-                                    <textarea required class="form-control" name="message" rows="3">{{ $comment->comment }}</textarea>
-                                    <small class="form-text text-muted">@lang('comments::comments.markdown_cheatsheet', ['url' => 'https://help.github.com/articles/basic-writing-and-formatting-syntax'])</small>
+                            @endcan
+                            @can('delete-comment', $comment)
+                                <a href="{{ url('comments/' . $comment->id) }}"
+                                   onclick="event.preventDefault();document.getElementById('comment-delete-form-{{ $comment->id }}').submit();"
+                                   class="btn btn-sm btn-link text-danger text-uppercase">Delete</a>
+                                <form id="comment-delete-form-{{ $comment->id }}"
+                                      action="{{ url('comments/' . $comment->id) }}" method="POST"
+                                      style="display: none;">
+                                    @method('DELETE')
+                                    @csrf
+                                </form>
+                            @endcan
+                        @endif
+                    </div>
+                    @can('edit-comment', $comment)
+                        <div class="modal fade" id="comment-modal-{{ $comment->id }}" tabindex="-1" role="dialog">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <form method="POST" action="{{ url('comments/' . $comment->id) }}">
+                                        @method('PUT')
+                                        @csrf
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Edit Comment</h5>
+                                            <button type="button" class="close" data-dismiss="modal">
+                                                <span>&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label for="message">Update your message here:</label>
+                                                <textarea required class="form-control" name="message"
+                                                          rows="3">{{ $comment->comment }}</textarea>
+                                                <small class="form-text text-muted"><a target="_blank"
+                                                                                       href="https://help.github.com/articles/basic-writing-and-formatting-syntax">Markdown</a>
+                                                    cheatsheet.</small>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button"
+                                                    class="btn btn-sm btn-outline-secondary text-uppercase"
+                                                    data-dismiss="modal">Cancel
+                                            </button>
+                                            <button type="submit" class="btn btn-sm btn-outline-success text-uppercase">
+                                                Update
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-sm btn-outline-secondary text-uppercase" data-dismiss="modal">@lang('comments::comments.cancel')</button>
-                                <button type="submit" class="btn btn-sm btn-outline-success text-uppercase">@lang('comments::comments.update')</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        @endcan
+                        </div>
+                    @endcan
 
-        @can('reply-to-comment', $comment)
-            <div class="modal fade" id="reply-modal-{{ $comment->getKey() }}" tabindex="-1" role="dialog">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <form method="POST" action="{{ route('comments.reply', $comment->getKey()) }}">
-                            @csrf
-                            <div class="modal-header">
-                                <h5 class="modal-title">@lang('comments::comments.reply_to_comment')</h5>
-                                <button type="button" class="close" data-dismiss="modal">
-                                <span>&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="form-group">
-                                    <label for="message">@lang('comments::comments.enter_your_message_here')</label>
-                                    <textarea required class="form-control" name="message" rows="3"></textarea>
-                                    <small class="form-text text-muted">@lang('comments::comments.markdown_cheatsheet', ['url' => 'https://help.github.com/articles/basic-writing-and-formatting-syntax'])</small>
+                    @can('reply-to-comment', $comment)
+                        <div class="modal fade" id="reply-modal-{{ $comment->id }}" tabindex="-1" role="dialog">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <form method="POST" action="{{ url('comments/' . $comment->id) }}">
+                                        @csrf
+                                        <div class="modal-header">
+                                            <h5 class="modal-title">Reply to Comment</h5>
+                                            <button type="button" class="close" data-dismiss="modal">
+                                                <span>&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <div class="form-group">
+                                                <label for="message">Enter your message here:</label>
+                                                <textarea required class="form-control" name="message"
+                                                          rows="3"></textarea>
+                                                <small class="form-text text-muted"><a target="_blank"
+                                                                                       href="https://help.github.com/articles/basic-writing-and-formatting-syntax">Markdown</a>
+                                                    cheatsheet.</small>
+                                            </div>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button"
+                                                    class="btn btn-sm btn-outline-secondary text-uppercase"
+                                                    data-dismiss="modal">Cancel
+                                            </button>
+                                            <button type="submit" class="btn btn-sm btn-outline-success text-uppercase">
+                                                Reply
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-sm btn-outline-secondary text-uppercase" data-dismiss="modal">@lang('comments::comments.cancel')</button>
-                                <button type="submit" class="btn btn-sm btn-outline-success text-uppercase">@lang('comments::comments.reply')</button>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                    @endcan
+
+                    <br/>{{-- Margin bottom --}}
+
+                    {{-- Recursion for children --}}
+                    @if($grouped_comments->has($comment->id))
+                        @foreach($grouped_comments[$comment->id] as $child)
+                            @include('comments::_comment', [
+                                'comment' => $child,
+                                'reply' => true,
+                                'grouped_comments' => $grouped_comments
+                            ])
+                        @endforeach
+                    @endif
+
                 </div>
-            </div>
-        @endcan
-
-        <br />{{-- Margin bottom --}}
-
-        <?php
-            if (!isset($indentationLevel)) {
-                $indentationLevel = 1;
-            } else {
-                $indentationLevel++;
-            }
-        ?>
-
-        {{-- Recursion for children --}}
-        @if($grouped_comments->has($comment->getKey()) && $indentationLevel <= $maxIndentationLevel)
-            {{-- TODO: Don't repeat code. Extract to a new file and include it. --}}
-            @foreach($grouped_comments[$comment->getKey()] as $child)
-                @include('comments::_comment', [
-                    'comment' => $child,
-                    'grouped_comments' => $grouped_comments
-                ])
-            @endforeach
-        @endif
-
+            @if(isset($reply) && $reply === true)
     </div>
-</div>
-
-{{-- Recursion for children --}}
-@if($grouped_comments->has($comment->getKey()) && $indentationLevel > $maxIndentationLevel)
-    {{-- TODO: Don't repeat code. Extract to a new file and include it. --}}
-    @foreach($grouped_comments[$comment->getKey()] as $child)
-        @include('comments::_comment', [
-            'comment' => $child,
-            'grouped_comments' => $grouped_comments
-        ])
-    @endforeach
+    @else
+    </li>
 @endif
