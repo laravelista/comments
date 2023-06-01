1213a5292926fe87074086f950e7d067e328dbe2
devsjohnny <johnnydevs@protonmail.com>
2023-06-01 12:23:49 +0200

    add_catalan_translations_laravel_10

---
+++ resources/lang/ca/comments.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'there_are_no_comments' => 'Encara no hi ha comentaris.',
+    'authentication_required' => 'Cal autenticació',
+    'you_must_login_to_post_a_comment' => 'Has d\'iniciar sessió per publicar un comentari.',
+    'log_in' => 'Iniciar Sessió',
+    'reply' => 'Respon',
+    'edit' => 'Edita',
+    'delete' => 'Suprimeix',
+    'edit_comment' => 'Edita el comentari',
+    'update_your_message_here' => 'Actualitza el teu missatge aquí:',
+    'update' => 'Actualitza',
+    'cancel' => 'Cancel·la',
+    'reply_to_comment' => 'Respon al comentari',
+    'markdown_cheatsheet' => 'Full de trucs <a target="_blank" href=":url">Markdown</a>.',
+    'submit' => 'Envia',
+    'your_message_is_required' => 'El vostre missatge és obligatori.',
+    'enter_your_message_here' => 'Introdueix el teu missatge aquí:',
+    'enter_your_email_here' => 'Introdueix el teu correu electrònic aquí:',
+    'enter_your_name_here' => 'Introduïu el vostre nom aquí:',
+];
